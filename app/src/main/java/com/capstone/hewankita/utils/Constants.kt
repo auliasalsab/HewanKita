1b8a24b37dbf435997e26df466eef8a86888f75a
BernardDog03 <rafifsuja03@gmail.com>
2022-11-30 18:23:42 +0700

    Constant, User and Utils

---
+++ app/src/main/java/com/capstone/hewankita/utils/Constants.kt
@@ -0,0 +1,37 @@
+package com.capstone.hewankita.utils
+
+class Constants {
+    companion object{
+
+        //Table Data
+        const val TABLE_DATA_USER = "User"
+        const val TABLE_DATA_SERVICE = "Services"
+
+        //Child Data Service
+        const val CHILD_SERVICE_CARE_SERVICE = "Care"
+        const val CHILD_SERVICE_DOCTOR_SERVICE = "Doctor Service"
+        const val CHILD_SERVICE_VACCINATION_SERVICE = "Vaccination Service"
+        const val CHILD_SERVICE_GROOMING_SERVICE = "Grooming Service"
+
+        //Data Service
+        const val CONST_SERVICE_OUTLET = "Outlet"
+        const val CONST_SERVICE_DATE = "Date"
+        const val CONST_SERVICE_TIME = "Time"
+        const val CONST_SERVICE_CHECK_IN = "Check In"
+        const val CONST_SERVICE_CHECK_OUT = "Check Out"
+        const val CONST_SERVICE_TIME_OA = "Time Of Arrival"
+
+        //Child Data User
+        const val CHILD_USER_CUSTOMER = "Customer"
+        const val CHILD_USER_PET_SHOP = "PetShop"
+
+        //Data User
+        const val CONST_USER_ID = "User ID"
+        const val CONST_USER_USERNAME = "Username"
+        const val CONST_USER_EMAIL = "Email"
+        const val CONST_USER_TYPE = "Type"
+        const val CONST_USER_PHONE = "Phone"
+        const val CONST_USER_IMG = "Profile Image"
+        const val CONST_USER_ADDRESS = "Address"
+    }
+}
---
+++ app/src/main/java/com/capstone/hewankita/utils/Users.kt
@@ -0,0 +1,8 @@
+package com.capstone.hewankita.utils
+
+data class Users (
+    val userId : String? = null,
+    val username: String? = null,
+    val userImage: String? = null,
+    val userEmail: String? = null
+)
